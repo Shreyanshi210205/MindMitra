3223416e4d6940b654914b20baa428a2e56f7027
Shreyanshi210205 <shreyanshi.230101129@iiitbh.ac.in>
2025-08-16 23:29:13 +0530

    Updated vercel link

--- mind-mitra/Backend/server.js
+++ mind-mitra/Backend/server.js
@@ -16,7 +16,7 @@ connectDB()
 const app=express();
 
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin:[ 'http://localhost:5173','https://mind-mitra-one.vercel.app/'], 
   credentials: true
 }));
 app.use(express.json());
